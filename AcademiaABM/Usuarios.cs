4faa039674fed87492149b9c4095c09c01c6a0a9
juampi74 <juampijaca@gmail.com>
2024-07-04 15:12:41 -0300

    Agregué un nuevo usuario de prueba

--- AcademiaABM/Usuarios.cs
+++ AcademiaABM/Usuarios.cs
@@ -19,6 +19,7 @@ namespace AcademiaABM
             listadoUsuarios.Add(new Usuario(45638464, "Nahuel Berli", "22/04/2004"));
             listadoUsuarios.Add(new Usuario(45656433, "Juan Pablo Jaca", "07/04/2004"));
             listadoUsuarios.Add(new Usuario(43842278, "Marcos Godoy", "09/08/2002"));
+            listadoUsuarios.Add(new Usuario(46787213, "Juan Perez", "21/02/2005"));
 
             dgvUsuarios.AutoGenerateColumns = true;
             dgvUsuarios.DataSource = listadoUsuarios;
